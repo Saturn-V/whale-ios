a911863e17876dd9bcaa5b00a16465dd0d4ddcf6
Alex Aaron Pena <alexaaronpena@gmail.com>
2017-03-22 15:06:14 -0700

    [UPDATE] Started signup process

---
+++ whale-ios-AlexPena/whale-ios-AlexPena/Networking/APIWrapper.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  whale-ios-AlexPena
+//
+//  Created by Alex Aaron Peña on 3/20/17.
+//  Copyright © 2017 Alex Aaron Peña. All rights reserved.
+//
+
+
+class APIWrapper {
+}
+
